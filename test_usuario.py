4bfe2665585704946a3641c346466f07de60b9c3
Itor Carlos <itor51carlos@gmail.com>
2025-06-12 10:47:34 -0300

    comentarios movidos para fora dos metodos

--- test_usuario.py
+++ test_usuario.py
@@ -8,16 +8,16 @@ class TestUsuario(unittest.TestCase):
     def setUp(self):
         self.usuario = Usuario(self.LOGIN_CORRETO, self.SENHA_CORRETA)
 
+    """
+    Testa se a exceçãoi do tipo ValueError é lançada quando a senha passada para função realizar_login da classe Usuario está incorreta
+    """
     def test_realizar_login_lanca_erro_com_senha_incorreta(self):
-        """
-        Testa se a exceçãoi do tipo ValueError é lançada quando a senha passada para função realizar_login da classe Usuario está incorreta
-        """
         self.assertRaises(ValueError,self.usuario.realizar_login,self.LOGIN_CORRETO,"senha_errada")
 
+    """
+    Testa se a exceçãoi do tipo ValueError é lançada quando o login passado para função realizar_login da classe Usuario está incorreta
+    """
     def test_realizar_login_lanca_erro_com_login_incorreto(self):
-        """
-        Testa se a exceçãoi do tipo ValueError é lançada quando o login passado para função realizar_login da classe Usuario está incorreta
-        """
         self.assertRaises(ValueError, self.usuario.realizar_login,"login_errado", self.SENHA_CORRETA)
 
 """Executa todos os métodos das classes derivadas da classe TestCase"""
