bc051bfadcc3cf75358831b048c09e7cd7b8b914
Itor Carlos <itor51carlos@gmail.com>
2025-06-13 18:46:00 -0300

    fix: erro de portugues no docstring

--- test_usuario.py
+++ test_usuario.py
@@ -9,13 +9,13 @@ class TestUsuario(unittest.TestCase):
         self.usuario = Usuario(self.LOGIN_CORRETO, self.SENHA_CORRETA)
 
     """
-    Testa se a exceçãoi do tipo ValueError é lançada quando a senha passada para função realizar_login da classe Usuario está incorreta
+    Testa se a exceção do tipo ValueError é lançada quando a senha passada para função realizar_login da classe Usuario está incorreta
     """
     def test_realizar_login_lanca_erro_com_senha_incorreta(self):
         self.assertRaises(ValueError,self.usuario.realizar_login,self.LOGIN_CORRETO,"senha_errada")
 
     """
-    Testa se a exceçãoi do tipo ValueError é lançada quando o login passado para função realizar_login da classe Usuario está incorreta
+    Testa se a exceção do tipo ValueError é lançada quando o login passado para função realizar_login da classe Usuario está incorreta
     """
     def test_realizar_login_lanca_erro_com_login_incorreto(self):
         self.assertRaises(ValueError, self.usuario.realizar_login,"login_errado", self.SENHA_CORRETA)
